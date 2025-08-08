5b93dcc29b1d712659ebca1e3c44e2ae21afbb47
Mahesh38857 <maheshkasle12@gmail.com>
2025-08-08 18:32:37 +0530

    Add files via upload

---
+++ Projects/Student Marks Prediction Project/flask11.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'WELCOME TO FLASK DEPLOYMENT'
+
+#app.run(host='0.0.0.0, port=81)
+
+# app.run(host = '127.0.0.1', port=5000)
+
+if __name__ == '__main__':
+    app.run(debug=True)
